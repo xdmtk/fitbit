ab2e331a7bc760ee0d87c8ed1b87fa9c4a5614cf
Nick <nick@xdmtk.org>
2018-12-24 01:26:35 -0800

    Attempt 1

--- pulsating-clockface/app/index.js
+++ pulsating-clockface/app/index.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+import document from "document"
+import clock from "clock"
+
+let bgImage = document.getElementById("background-frame");
+let frameNumber = 0;
+let frameString = "";
+/*
+setInterval(function() {
+    if (frameNumber < 31) {
+        frameString = "pulse/p-" + frameNumber + ".jpg";
+        bgImage.image = frameString;
+        frameNumber += 1;
+    }
+    else {
+        frameNumber = 0;
+        frameString = "pulse/p-" + frameNumber + ".jpg";
+        frameNumber += 1;
+    }
+}, 50);
+*/
+
