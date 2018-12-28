8a1ccf6daa7f844daaeda0e17bb7744d898db5a5
Nick Martinez <nick@xdmtk.org>
2018-12-27 20:11:59 -0800

    good pulse

--- pulsating-clockface/app/index.js
+++ pulsating-clockface/app/index.js
@@ -1,36 +1,56 @@
-import document from "document"
-import clock from "clock"
+import document from "document";
+import { display } from "display";
+import clock from "clock";
 
-let bgImage = document.getElementById("background-frame1");
+let bgImage = document.getElementById("background-frame");
 let frameNumber = 0;
 let frameString = "";
-/*
-bgImage.style.opacity = 1;
-setInterval(function() {
-    if (frameNumber < 31) {
-        frameString = "pulse/p-" + frameNumber + ".jpg";
-        bgImage.image = frameString;
-        frameNumber += 1;
-    }
-    else {
-        frameNumber = 0;
-        frameString = "pulse/p-" + frameNumber + ".jpg";
-        frameNumber += 1;
-    }
-}, 50);
-*/
-setInterval(function() {
-    var front = document.getElementById("background-frame"+(frameNumber+1).toString());
-    if (frameNumber === 0) {
-        var back = document.getElementById("background-frame32");
-    }
-    else {
-        var back = document.getElementById("background-frame"+frameNumber.toString());
-    }
-    front.style.opacity = 1;
-    back.style.opacity = 0;
-    frameNumber += 1;
-    if (frameNumber === 32) {
-        frameNumber = 0;
-    }
-}, 70);
+let f = true;
+var expire = false;
+var ticker = false;
+var pulser;
+clock.granularity = 'seconds';
+
+
+
+clock.ontick = function () {
+	if (!ticker) {
+		pulser = setInterval(function() {
+			pulsate();
+		}, 50);
+	}
+}
+
+
+
+
+
+
+
+
+function pulsate() {
+	if (display.on) {
+		ticker = true;
+		if (frameNumber < 31 && f) {
+			frameNumber += 1;
+		}
+		else if (frameNumber > 0 && !f) {
+			frameNumber -= 1;
+		}
+		else {
+			if (f) {
+				f = false;
+			} 
+			else {
+				f = true;
+			}
+		}
+		frameString = "pulse/p-" + frameNumber + ".png";
+		bgImage.image = frameString;
+		console.log(frameString);
+	}
+	else {
+		clearInterval(pulser);
+		ticker = false;
+	}
+}
