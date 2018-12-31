e7d1fdb33f7936e955cfb060949c3713913e1f68
Nick Martinez <nick@xdmtk.org>
2018-12-31 02:10:44 -0800

    begin wakeup time

--- calorie-calculator/companion/index.js
+++ calorie-calculator/companion/index.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+import { createRequest } from "endpoint";
+import { me } from "companion";
+
+const MIN_MULTIPLIER = 1000 * 60;
+const HOUR_MULTIPLIER = 60;
+const WAKEUP_TIMER = 24 * HOUR_MULTIPLIER * MIN_MULTIPLIER;
+
+me.wakeInterval = WAKEUP_TIMER;
+
+
+function main() {
+	const endpoint = createRequest("nick",
+
+	
+
+
+
+
+}
