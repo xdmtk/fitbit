891c90fe8781b61a2b429f998ad2d16bc11de9df
Nick Martinez <nick@xdmtk.org>
2019-04-10 01:54:37 -0700

    Fix GUI

--- calorie-calculator/app/index.js
+++ calorie-calculator/app/index.js
@@ -1,41 +1,10 @@
 import * as fs from "fs";
+
 import document from "document";
 import clock from "clock";
 import * as messaging from "messaging";
 import { today } from "user-activity";
 import { vibration } from "haptics";
-
-
-
-const dateText = document.getElementById("date-title");
-const elemCalories = document.getElementById("total-cals");
-const text100 = document.getElementById("hundred");
-const text50 = document.getElementById("fifty");
-const text10 = document.getElementById("ten");
-
-const fill100 = document.getElementById("hundredf");
-const fill50 = document.getElementById("fiftyf");
-const fill10 = document.getElementById("tenf");
-
-const bord100 = document.getElementById("hundredb");
-const bord50 = document.getElementById("fiftyb");
-const bord10 = document.getElementById("tenb");
-
-const buttonPm = document.getElementById("plus-button");
-const bordbuttonPm = document.getElementById("plus-button");
-const fillbuttonPm = document.getElementById("plus-buttonf");
-
-const loginForm = document.getElementsByClassName("login");
-
-const xeniaf = document.getElementById("xenia-login-buttonf");
-const xenia = document.getElementById("xenia-login-button");
-const xeniat = document.getElementById("name-user-xenia");
-const xeniac = document.getElementById("click-area-xenia");
-
-const nickf = document.getElementById("nick-login-buttonf");
-const nick = document.getElementById("nick-login-button");
-const nickt = document.getElementById("name-user-nick");
-const nickc = document.getElementById("click-area-nick");
 
 
 var plusMin;
@@ -49,25 +18,26 @@ var inputDate;
 
 
 function main() {
-	if (debug) {
-		debugUser();
-	}
-	checkUser();
-	loadCaloricData();
-	alignTotalCals();
-	setupButtonHandlers();
-	
 	clock.granularity = 'hours';
 	clock.ontick = function (evt) {
 		datevar = evt.date;
-		setDateStr();
+	//	setDateStr();
 	}
+
+//	loadCaloricData();
+	
 }
 
 /* Main Calorie Load */
 
 function loadCaloricData() {
-	let data = fs.readFileSync("cd.txt", "ascii");
+    let data;
+    try {
+        data = fs.readFileSync("cd.txt", "ascii");
+    }
+    catch {
+        fs.writeFileSync('cd.txt', '', 'ascii');
+    }
 	let dataFields = data.split(",");
 
 	if (dataFields.length > 1) {
@@ -89,23 +59,6 @@ function loadCaloricData() {
 		totalCals = 0;
 	}
 	elemCalories.text = totalCals;
-}
-
-
-function alignTotalCals() {
-	if (totalCals < 10) {
-		elemCalories.x = 109;
-	}
-	else if (totalCals < 100) {
-		elemCalories.x = 99;
-	}
-	else if (totalCals < 1000) {
-		elemCalories.x = 79;
-	}
-	else {
-		elemCalories.x = 59;
-	}
-
 }
 
 
@@ -187,107 +140,6 @@ function addSubtractMod(action) {
 
 
 
-
-/* Event Handlers */
-
-function setupButtonHandlers() {
-	addSubtractMod("read");	
-
-
-	text100.onclick = function() { addSubtractCals(100); }
-	text50.onclick =  function() { addSubtractCals(50); }
-	text10.onclick = function() { addSubtractCals(10); }
-
-
-	fill100.onclick = function() { addSubtractCals(100); }
-	fill50.onclick =  function() { addSubtractCals(50); }
-	fill10.onclick = function() { addSubtractCals(10); }
-
-
-	bord100.onclick = function() { addSubtractCals(100); }
-	bord50.onclick =  function() { addSubtractCals(50); }
-	bord10.onclick = function() { addSubtractCals(10); }
-
-	buttonPm.onclick = function() { addSubtractMod("flip"); }
-	bordbuttonPm.onclick = function() { addSubtractMod("flip"); }
-	fillbuttonPm.onclick = function() { addSubtractMod("flip"); }
-
-	nick.onclick = function() { login("nick"); }
-	nickf.onclick = function() { login("nick"); }
-	nickt.onclick = function() {  login("nick"); }
-	nickc.onclick = function() {  login("nick"); }
-
-	xenia.onclick = function() { login("xenia"); }
-	xeniaf.onclick = function() { login("xenia"); }
-	xeniat.onclick =function() { login("xenia"); }
-	xeniac.onclick =function() { login("xenia"); }
-
-
-}
-
-
-/* Login Functions */
-
-function login(userParam) {
-	if (!loggingIn) {
-		console.log("in login with " + userParam);
-		user = userParam;
-		if (userParam === "nick") {
-			nickf.fill = "white";
-			setTimeout(function() {
-				nickf.fill = "black";
-			}, 20);
-		}
-		else {
-			xeniaf.fill = "white";
-			setTimeout(function() {
-				xeniaf.fill = "black";
-			}, 20);
-		}
-
-		loggingIn = !loggingIn;
-		var u = userParam + "";
-		fs.writeFileSync("user.txt", u, "ascii");
-		let fader = setInterval(function () {
-			loginForm.forEach(function(elem) {
-				elem.opacity -= .1;
-			});
-		}, 100);
-		setTimeout(function () {
-			clearInterval(fader);
-			loginForm.forEach(function(elem) {
-				elem.style.display = "none";
-			});
-		}, 100);
-		setDateStr();
-	}
-	else {
-		console.log("handler already activated");
-	}
-}
-
-function checkUser() {
-	console.log("in checkuser");
-	try {
-		if (fs.readFileSync("user.txt", "ascii") === "") {
-			loginForm.forEach(function(elem) {
-				elem.style.display = "inherit";
-			});
-		}
-		else {
-			loginForm.forEach(function(elem) {
-				elem.style.display = "none";
-			});
-			user = fs.readFileSync("user.txt", "ascii");
-		}
-	}
-	catch {
-		loginForm.forEach(function(elem) {
-			elem.style.display = "inherit";
-		});
-	}
-
-}
 
 
 function setupMessaging() {
